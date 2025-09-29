5427904f0dace967c327300dfbdae166760f9959
Sarunwit00 <a.sarunwit@gmail.com>
2025-09-29 10:51:05 +0700

    workpage52

--- local/src/App.js
+++ local/src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import './App.css';
 import RestBasic from './rest-basic';
 import FormGet from './form-get';
+import FormPost from './form-post';
 
 function App() {
-  return <FormGet/>
+  return <FormPost/>
 }
 
 export default App;
